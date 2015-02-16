508a9b67c304755ea5d1c0ac8fbd5a410026c207
Samuel De Backer <sdebacker@gmail.com>
2015-02-16 18:27:34 +0100

    blocks validation

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -8,7 +8,7 @@ class FormRequest extends AbstractFormRequest {
     public function rules()
     {
         $rules = [
-            'name' => 'required|alpha_dash|unique:blocks',
+            'name' => 'required|alpha_dash|unique:blocks,name,' . $this->get('id'),
         ];
         return $rules;
     }
