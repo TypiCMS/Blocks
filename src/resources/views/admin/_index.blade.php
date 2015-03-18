4b42dcb89c44a92555f3539be52a8fa3f1fd7f5c
Samuel De Backer <sdebacker@gmail.com>
2015-03-18 23:10:50 +0100

    _index.blade

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ url }}/create" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="{{ models.length }} content blocks">{{ models.length }} content block</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
+        <span translate translate-n="models.length" translate-plural="@{{ models.length }} content blocks">@{{ models.length }} content block</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -21,10 +21,10 @@
                 <tr>
                     <td colspan="3"></td>
                     <td>
-                        <input st-search="name" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="name" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                     <td>
-                        <input st-search="body" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="body" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                 </tr>
             </thead>
@@ -34,8 +34,8 @@
                     <td><typi-btn-delete ng-click="delete(model, model.name)"></typi-btn-delete></td>
                     <td typi-btn-edit></td>
                     <td typi-btn-status></td>
-                    <td>{{ model.name }}</td>
-                    <td>{{ model.body }}</td>
+                    <td>@{{ model.name }}</td>
+                    <td>@{{ model.body }}</td>
                 </tr>
             </tbody>
             <tfoot>
