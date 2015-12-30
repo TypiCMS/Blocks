c8b5e86f9ecb7454d5515583c77571db6a1743f2
Samuel De Backer <sdebacker@gmail.com>
2015-12-30 15:01:32 +0100

    api update and destroy methods

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace TypiCMS\Modules\Blocks\Http\Controllers;
 
 use Illuminate\Support\Facades\Request;
+use TypiCMS\Modules\Blocks\Models\Block;
 use TypiCMS\Modules\Blocks\Repositories\BlockInterface as Repository;
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
 
@@ -36,12 +37,28 @@ class ApiController extends BaseApiController
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function update($model)
+    public function update()
     {
-        $error = $this->repository->update(Request::all()) ? false : true;
+        $updated = $this->repository->update(Request::all());
 
         return response()->json([
-            'error' => $error,
-        ], 200);
+            'error' => !$updated,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \TypiCMS\Modules\Blocks\Models\Block $block
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Block $block)
+    {
+        $deleted = $this->repository->delete($block);
+
+        return response()->json([
+            'error' => !$deleted,
+        ]);
     }
 }
