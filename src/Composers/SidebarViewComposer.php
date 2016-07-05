c30a51f3f5dfed8bf49ced47ca69a3d4cb602af2
Samuel De Backer <sdebacker@gmail.com>
2016-07-05 10:25:59 +0200

    sidebar item id

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -13,6 +13,7 @@ class SidebarViewComposer
     {
         $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
             $group->addItem(trans('blocks::global.name'), function (SidebarItem $item) {
+                $item->id = 'blocks';
                 $item->icon = config('typicms.blocks.sidebar.icon', 'icon fa fa-fw fa-list-alt');
                 $item->weight = config('typicms.blocks.sidebar.weight');
                 $item->route('admin::index-blocks');
