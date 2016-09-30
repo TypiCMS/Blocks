b86a2933400f4e2aaa69e67d9f91cce5bd4bb9f1
Samuel De Backer <sdebacker@gmail.com>
2016-09-30 10:57:14 +0200

    update method

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -77,7 +77,7 @@ class AdminController extends BaseAdminController
      */
     public function update(Block $block, FormRequest $request)
     {
-        $this->repository->update($request->all());
+        $this->repository->update(request('id'), $request->all());
 
         return $this->redirect($request, $block);
     }
