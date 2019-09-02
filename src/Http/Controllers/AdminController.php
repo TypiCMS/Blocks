573e83d9e6fbdc50f1f0aa6a0d86e9846f56c44d
Samuel De Backer <sdebacker@gmail.com>
2019-09-02 16:54:01 +0200

    Update AdminController.php

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -17,7 +17,7 @@ class AdminController extends BaseAdminController
 
     public function create(): View
     {
-        $model = new Block;
+        $model = new Block();
 
         return view('blocks::admin.create')
             ->with(compact('model'));
