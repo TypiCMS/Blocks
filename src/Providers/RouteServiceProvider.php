f321cd037e988bf088e8a05e9435dc7de22e15bb
Samuel De Backer <sdebacker@gmail.com>
2020-09-17 23:48:55 +0200

    Callable syntax for controller route declarations.

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -5,18 +5,11 @@ namespace TypiCMS\Modules\Blocks\Providers;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use TypiCMS\Modules\Blocks\Http\Controllers\AdminController;
+use TypiCMS\Modules\Blocks\Http\Controllers\ApiController;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to the controller routes in your routes file.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'TypiCMS\Modules\Blocks\Http\Controllers';
-
     /**
      * Define the routes for the application.
      */
@@ -27,11 +20,11 @@ class RouteServiceProvider extends ServiceProvider
              * Admin routes
              */
             $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('blocks', 'AdminController@index')->name('admin::index-blocks')->middleware('can:read blocks');
-                $router->get('blocks/create', 'AdminController@create')->name('admin::create-block')->middleware('can:create blocks');
-                $router->get('blocks/{block}/edit', 'AdminController@edit')->name('admin::edit-block')->middleware('can:update blocks');
-                $router->post('blocks', 'AdminController@store')->name('admin::store-block')->middleware('can:create blocks');
-                $router->put('blocks/{block}', 'AdminController@update')->name('admin::update-block')->middleware('can:update blocks');
+                $router->get('blocks', [AdminController::class, 'index'])->name('admin::index-blocks')->middleware('can:read blocks');
+                $router->get('blocks/create', [AdminController::class, 'create'])->name('admin::create-block')->middleware('can:create blocks');
+                $router->get('blocks/{block}/edit', [AdminController::class, 'edit'])->name('admin::edit-block')->middleware('can:update blocks');
+                $router->post('blocks', [AdminController::class, 'store'])->name('admin::store-block')->middleware('can:create blocks');
+                $router->put('blocks/{block}', [AdminController::class, 'update'])->name('admin::update-block')->middleware('can:update blocks');
             });
 
             /*
@@ -39,9 +32,9 @@ class RouteServiceProvider extends ServiceProvider
              */
             $router->middleware('api')->prefix('api')->group(function (Router $router) {
                 $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('blocks', 'ApiController@index')->middleware('can:read blocks');
-                    $router->patch('blocks/{block}', 'ApiController@updatePartial')->middleware('can:update blocks');
-                    $router->delete('blocks/{block}', 'ApiController@destroy')->middleware('can:delete blocks');
+                    $router->get('blocks', [ApiController::class, 'index'])->middleware('can:read blocks');
+                    $router->patch('blocks/{block}', [ApiController::class, 'updatePartial'])->middleware('can:update blocks');
+                    $router->delete('blocks/{block}', [ApiController::class, 'destroy'])->middleware('can:delete blocks');
                 });
             });
         });
