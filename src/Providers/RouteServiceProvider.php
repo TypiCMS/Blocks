4b793f5d4d202314a5486a74e1d53022e86f036d
Samuel De Backer <sdebacker@gmail.com>
2020-09-20 22:37:55 +0200

    Routes definition cleaned

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -15,28 +15,24 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        Route::namespace($this->namespace)->group(function (Router $router) {
-            /*
-             * Admin routes
-             */
-            $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('blocks', [AdminController::class, 'index'])->name('admin::index-blocks')->middleware('can:read blocks');
-                $router->get('blocks/create', [AdminController::class, 'create'])->name('admin::create-block')->middleware('can:create blocks');
-                $router->get('blocks/{block}/edit', [AdminController::class, 'edit'])->name('admin::edit-block')->middleware('can:read blocks');
-                $router->post('blocks', [AdminController::class, 'store'])->name('admin::store-block')->middleware('can:create blocks');
-                $router->put('blocks/{block}', [AdminController::class, 'update'])->name('admin::update-block')->middleware('can:update blocks');
-            });
+        /*
+         * Admin routes
+         */
+        Route::middleware('admin')->prefix('admin')->name('admin::')->group(function (Router $router) {
+            $router->get('blocks', [AdminController::class, 'index'])->name('index-blocks')->middleware('can:read blocks');
+            $router->get('blocks/create', [AdminController::class, 'create'])->name('create-block')->middleware('can:create blocks');
+            $router->get('blocks/{block}/edit', [AdminController::class, 'edit'])->name('edit-block')->middleware('can:read blocks');
+            $router->post('blocks', [AdminController::class, 'store'])->name('store-block')->middleware('can:create blocks');
+            $router->put('blocks/{block}', [AdminController::class, 'update'])->name('update-block')->middleware('can:update blocks');
+        });
 
-            /*
-             * API routes
-             */
-            $router->middleware('api')->prefix('api')->group(function (Router $router) {
-                $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('blocks', [ApiController::class, 'index'])->middleware('can:read blocks');
-                    $router->patch('blocks/{block}', [ApiController::class, 'updatePartial'])->middleware('can:update blocks');
-                    $router->delete('blocks/{block}', [ApiController::class, 'destroy'])->middleware('can:delete blocks');
-                });
-            });
+        /*
+         * API routes
+         */
+        Route::middleware(['api', 'auth:api'])->prefix('api')->group(function (Router $router) {
+            $router->get('blocks', [ApiController::class, 'index'])->middleware('can:read blocks');
+            $router->patch('blocks/{block}', [ApiController::class, 'updatePartial'])->middleware('can:update blocks');
+            $router->delete('blocks/{block}', [ApiController::class, 'destroy'])->middleware('can:delete blocks');
         });
     }
 }
